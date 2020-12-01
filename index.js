843ba4a9a3f733ac87b219dd8cae47e811c9136a
Anita Dan <anita@Anitas-Air.attlocal.net>
2020-12-01 16:51:57 -0500

    Adding

--- index.js
+++ index.js
@@ -16,3 +16,27 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId + "\n");
 });
 
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please insert a name of new department',
+        },
+    ])
+        .then(response => {
+            let sqlQuery = 'INSERT INTO department SET ?';
+            let dep = {
+                name: response.name,
+            };
+            connection.query(sqlQuery, dep,
+                (error, results) => {
+                    if (error) throw error;
+                    console.log('New department has been successfully created');
+                });
+            connection.end();
+
+        })
+
+
+}
