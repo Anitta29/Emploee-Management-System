53886e4d29569e86bb155b846b7775a106e3a0d2
Anitta29 <dananiuta@gmail.com>
2021-01-06 19:45:58 -0600

    final touch

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ const viewDepartment = () => {
     connection.query(sqlQuery,
         (error, results) => {
             if (error) throw error;
-            console.log(results);
+            console.table(results);
         });
     connection.end();
 };
@@ -158,14 +158,13 @@ function deleteDepartment() {
 
                 });
         });
-    
+
 };
 function addRole() {
     let sqlQuery = 'SELECT * FROM department';
     connection.query(sqlQuery,
         (error, results) => {
             if (error) throw error;
-            console.log(results);
             const departments = results.map(result => ({
                 name: result.name,
                 value: result.id,
@@ -213,51 +212,150 @@ const viewRole = () => {
     connection.end();
 };
 
-function addEmployee() {
-
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'employeeFirstName',
-            message: 'What is the first name of employee?',
-        },
-        {
-            type: 'input',
-            name: 'employeeLastName',
-            message: 'What is the last name of employee?',
-        },
-    ])
+function deleteRole() {
     let sqlQuery = 'SELECT * FROM role';
     connection.query(sqlQuery,
         (error, results) => {
             if (error) throw error;
-            console.log(results);
-            const roles = results.map(result => ({
-                name: result.name,
-                value: result.id,
-            }))
-            let roleId = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'roleId',
-            message: 'Which role ?',
-            choices: roles
-        },
-        
-    ]).then(response => {
-            let sqlQuery = 'INSERT INTO employee SET ?';
-            let employee = {
 
-                name: response.name,
-            };
-            employee.role_id = roleId,
-            connection.query(sqlQuery, employee,
+            const roles = [{
+                name: "none",
+                value: null,
+            },
+            ...results.map(result => ({
+                name: result.title,
+                value: result.id,
+
+            }))];
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'id',
+                    message: "Which role you want to remove?",
+                    choices: roles,
+                }
+            ]).then(response => {
+                let sqlQuery = 'DELETE FROM role WHERE id = ?'
+                connection.query(sqlQuery, response.id, (error, results) => {
+                    if (error) throw error;
+                    console.log("Role is removed")
+                    connection.end()
+                })
+            })
+
+
+        }
+    )
+}
+function addEmployee() {
+    const roles = 'SELECT * FROM role'
+    const employees = 'SELECT * FROM employee'
+
+    connection.query(roles,
+        (error, results) => {
+            if (error) throw error;
+            const roles = results.map(result => ({
+                name: result.title,
+                value: result.id,
+
+            }))
+            connection.query(employees,
                 (error, results) => {
                     if (error) throw error;
-                    console.log('New employee was added to the department');
+
+                    const manager = results.map(employee => ({
+                        name: employee.first_name + " " + employee.last_name,
+                        value: employee.id
+                    }))
+                    manager.push({
+                        name: "none",
+                        value: null,
+                    })
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'first_name',
+                            message: 'What is the first name of employee?',
+                        },
+                        {
+                            type: 'input',
+                            name: 'last_name',
+                            message: 'What is the last name of employee?',
+                        },
+                        {
+                            type: 'list',
+                            name: 'manager_id',
+                            message: 'Select employee manager',
+                            choices: manager
+                        },
+                        {
+                            type: 'list',
+                            name: 'role_id',
+                            message: 'Select employee\'s role',
+                            choices: roles
+                        }
+                    ]).then(response => {
+                        let sqlQuery = 'INSERT INTO employee SET ?';
+                        connection.query(sqlQuery, response,
+                            (error, results) => {
+                                if (error) throw error;
+                                console.log('New employee was added to the department');
+                            });
+                        connection.end();
+                    })
                 });
+        });
+}
+
+function viewEmployee() {
+
+    const sqlQuery = `SELECT 
+employee.id, 
+employee.first_name AS 'First Name',
+employee.last_name AS 'Last Name',
+role.title AS Title,
+department.name AS Department,
+IFNULL(CONCAT (manager.first_name, ' ' , manager.last_name), '--------' )
+AS Manager FROM Employee
+LEFT JOIN role on employee.role_id = role.id
+LEFT JOIN department on role.department_id = department.id
+LEFT JOIN employee manager on employee.manager_id = manager.id
+`
+    connection.query(sqlQuery,
+        (error, results) => {
+            if (error) throw error;
+            console.table(results);
             connection.end();
+        });
+
+}
+function deleteEmployee() {
+    let sqlQuery = 'SELECT * FROM employee'
+    connection.query(sqlQuery,
+        (error, results) => {
+            if (error) throw error;
+            const employees = results.map(result => ({
+                name: result.first_name + " " + result.last_name,
+                value: result.id,
+            }))
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'id',
+                    message: 'Who would like to remove from employees?',
+                    choices: employees
+                },
+            ]).then(response => {
+                let sqlQuery = 'DELETE FROM employee WHERE id = ?';
+
+                connection.query(sqlQuery, response.id,
+                    (error, results) => {
+                        if (error) throw error;
+                        console.log('Employee was successfully removed!');
+                        connection.end();
+                    });
+            });
+
 
         });
-});
 }
